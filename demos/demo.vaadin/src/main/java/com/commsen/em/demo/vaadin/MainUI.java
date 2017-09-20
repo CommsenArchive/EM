f117f5163a6cebc7d4d3ed1c25fd563bd4438e6b
Milen Dyankov <milendyankov@gmail.com>
2017-09-20 23:48:14 +0200

    Vaadin demo

---
+++ demos/demo.vaadin/src/main/java/com/commsen/em/demo/vaadin/MainUI.java
@@ -0,0 +1,22 @@
+package com.commsen.em.demo.vaadin;
+
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.shared.ui.ContentMode;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
+
+public class MainUI extends UI {
+
+	@Override
+	protected void init(VaadinRequest request) {
+		// The root of the component hierarchy
+		VerticalLayout content = new VerticalLayout();
+		content.setSizeFull(); // Use entire window
+		setContent(content); // Attach to the UI
+
+		// Add some component
+		content.addComponent(new Label("<b>Hello!</b> - How are you?", ContentMode.HTML));
+
+	}
+}
---
+++ demos/demo.vaadin/src/main/java/com/commsen/em/demo/vaadin/package-info.java
@@ -0,0 +1,6 @@
+@Export
+@Version("1.0.0")
+package com.commsen.em.demo.vaadin;
+
+import org.osgi.annotation.versioning.Version;
+import aQute.bnd.annotation.Export;
