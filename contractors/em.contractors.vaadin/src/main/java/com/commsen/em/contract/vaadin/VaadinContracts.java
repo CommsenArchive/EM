62ab4055152db38e5cb41a7080708d1ec5a4fb81
Milen Dyankov <milendyankov@gmail.com>
2017-09-20 23:47:37 +0200

    Fix Vaadin's servlet contract

--- contractors/em.contractors.vaadin/src/main/java/com/commsen/em/contract/vaadin/VaadinContracts.java
+++ contractors/em.contractors.vaadin/src/main/java/com/commsen/em/contract/vaadin/VaadinContracts.java
@@ -2,10 +2,11 @@ package com.commsen.em.contract.vaadin;
 
 import com.commsen.em.annotations.Provides;
 import com.commsen.em.annotations.Requires;
+import com.commsen.em.annotations.RequiresServletContainer;
 
 @Provides(value="vaadin", version="8.1.3")
 @Requires(raw="osgi.identity;filter:='(&(osgi.identity=com.vaadin.osgi.integration)(version=8.1.3))'")
 @Requires(raw="osgi.identity;filter:='(&(osgi.identity=com.vaadin.themes)(version=8.1.3))'")
 @Requires(raw="osgi.identity;filter:='(&(osgi.identity=com.vaadin.client-compiled)(version=8.1.3))'")
-@Requires(filter="(&(em.contract=servlet.container)(whiteboard=true)(version=6))")
+@RequiresServletContainer(version="3.0")
 public class VaadinContracts {}
