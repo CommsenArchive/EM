4faa6cc60d1617fb88c76875260bc00d3b3b4b0e
Milen Dyankov <milendyankov@gmail.com>
2017-09-30 14:04:29 +0200

    add fragment and activator demo

---
+++ demos/demo.extension/demo.extension.fragment/src/main/java/com/commsen/em/demo/extension/fragment/FragmentActivator.java
@@ -0,0 +1,25 @@
+package com.commsen.em.demo.extension.fragment;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+import com.commsen.em.annotations.Activator;
+import com.commsen.em.annotations.Provides;
+import com.commsen.em.annotations.RuntimeExtension;
+
+
+@RuntimeExtension
+@Activator(extension=true)
+@Provides("DemoFramworkExtension")
+public class FragmentActivator implements BundleActivator {
+
+	@Override
+	public void start(BundleContext context) throws Exception {
+		System.setProperty("em.demo.custom.system.property", "success");
+	}
+
+	@Override
+	public void stop(BundleContext context) throws Exception {
+	}
+
+}
---
+++ demos/demo.extension/demo.extension.runtime/src/main/java/com/commsen/em/demo/extension/runtime/ModuleActivator.java
@@ -0,0 +1,22 @@
+package com.commsen.em.demo.extension.runtime;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+import com.commsen.em.annotations.Activator;
+import com.commsen.em.annotations.Requires;
+
+@Activator
+@Requires("DemoFramworkExtension")
+public class ModuleActivator implements BundleActivator {
+
+	@Override
+	public void start(BundleContext context) throws Exception {
+		System.out.println("Testing system property added by framework extension: " + System.getProperty("em.demo.custom.system.property"));
+	}
+
+	@Override
+	public void stop(BundleContext context) throws Exception {
+	}
+
+}
