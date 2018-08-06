a30b45630b5c664d62841e4ffc3859b4f38589df
Milen Dyankov <milendyankov@gmail.com>
2018-08-06 19:49:53 +0200

    Remove incompatible webconsole plugins and exclude webconsole itself from plugin dependencies

--- contractors/em.contractors.webconsole/src/main/java/com/commsen/em/contractors/webconsole/WebConsoleContracts.java
+++ contractors/em.contractors.webconsole/src/main/java/com/commsen/em/contractors/webconsole/WebConsoleContracts.java
@@ -13,11 +13,8 @@ import com.commsen.em.annotations.RequiresServletContainer;
 @Requires(ns="osgi.identity", filter="(&(osgi.identity=org.apache.felix.webconsole.plugins.packageadmin))")
 @Requires(ns="osgi.identity", filter="(&(osgi.identity=org.apache.felix.webconsole.plugins.memoryusage))")
 @Requires(ns="osgi.identity", filter="(&(osgi.identity=org.apache.felix.webconsole.plugins.scriptconsole))")
-//@Requires(ns="osgi.identity", filter="(&(osgi.identity=org.apache.felix.webconsole.plugins.subsystems))")
-//@Requires(ns="osgi.identity", filter="(&(osgi.identity=org.apache.felix.webconsole.plugins.upnp))")
-//@Requires(ns="osgi.identity", filter="(&(osgi.identity=org.apache.karaf.webconsole.http))")
-//@Requires(ns="osgi.identity", filter="(&(osgi.identity=osgi.enroute.webconsole.xray.provider))")
-//@Requires(ns="osgi.identity", filter="(&(osgi.identity=org.everit.osgi.webconsole.threadviewer))")
+@Requires(ns="osgi.identity", filter="(&(osgi.identity=org.apache.karaf.webconsole.http))")
+@Requires(ns="osgi.identity", filter="(&(osgi.identity=org.everit.osgi.webconsole.threadviewer))")
 @RequiresServletContainer(version="2.5")
 public class WebConsoleContracts {}
 
